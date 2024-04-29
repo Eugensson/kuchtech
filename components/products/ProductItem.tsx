f5b1e0477d4e2f899994ababcdef40227d584d76
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2024-04-29 19:58:29 +0300

    Update ProductItem.tsx

--- components/products/ProductItem.tsx
+++ components/products/ProductItem.tsx
@@ -5,21 +5,23 @@ import { Product } from "@/lib/models/ProductModel";
 
 export default function ProductItem({ product }: { product: Product }) {
   return (
-    <article className="cursor-pointer hover:shadow-xl">
+    <article className="cursor-pointer rounded-md overflow-hidden hover:shadow-xl">
       <Link href={`/product/${product.slug}`}>
         <figure>
           <Image
             src={product.image}
             alt={product.name}
-            width={300}
-            height={300}
-            className="object-cover h-64 w-full"
+            width={200}
+            height={200}
+            className="object-cover h-60 w-full"
           />
         </figure>
-        <div className="p-1 flex flex-col gap-1 font-semibold">
-          <h2 className="text-xl">{product.name}</h2>
-          <p className="text-zinc-500">{product.brand}</p>
-          <span>&#8372; {product.price}</span>
+        <div className="py-3 px-5 flex flex-col gap-2 font-semibold bg-zinc-100/10">
+          <h2 className="text-lg">{product.name}</h2>
+          <div className="flex items-center justify-between">
+            <span>Ціна, &#8372;</span>
+            <span>{product.price}</span>
+          </div>
         </div>
       </Link>
     </article>
