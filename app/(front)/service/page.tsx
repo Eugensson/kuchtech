733e459a7d7e977bac2119873fe53d3098292fd8
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2024-05-06 21:15:58 +0300

    Updated About section

--- app/(front)/service/page.tsx
+++ app/(front)/service/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 import { FaRegCheckSquare } from "react-icons/fa";
 
 import { Devider } from "@/components/Devider";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "KuchTech | Сервіс",
@@ -11,36 +12,36 @@ export const metadata: Metadata = {
 
 const Service = () => {
   return (
-    <section>
+    <section className="flex flex-col gap-2 text-xs md:text-sm lg:text-base">
       <Devider text="Сервіс" />
-      <p className="mb-2">
+      <p>
         Сервісний центр компанії забезпечує гарантійне та післягарантійне
         обслуговування техніки торгових марок Fagor (Іспанія), Apach (Італія),
         Tatra (Туреччина), Hurakan (Китай), зокрема:
       </p>
-      <p className="flex items-center gap-2 mb-2">
-        <FaRegCheckSquare />
+      <p className="flex items-center gap-2">
+        <FaRegCheckSquare className="min-w-3 min-h-3" />
         Професійний монтаж за найкоротшим терміном.
       </p>
-      <p className="flex items-center gap-2 mb-2">
-        <FaRegCheckSquare />
+      <p className="flex items-center gap-2">
+        <FaRegCheckSquare className="min-w-3 min-h-3" />
         Обслуговування як нового, так і вживаного обладнання
       </p>
-      <p className="flex items-center gap-2 mb-2">
-        <FaRegCheckSquare />
+      <p className="flex items-center gap-2">
+        <FaRegCheckSquare className="min-w-3 min-h-3" />
         Гарантійні зобов&apos;язання та ремонт у позагарантійних випадках
       </p>
-      <p className="flex items-center gap-2 mb-2">
-        <FaRegCheckSquare />
+      <p className="flex items-center gap-2">
+        <FaRegCheckSquare className="min-w-3 min-h-3" />
         Консультативна підтримка, розробка проектів &#34;під ключ&#34; та їх
         впровадження в експлуатацію.
       </p>
-      <p className="flex items-center gap-2 mb-2">
-        <FaRegCheckSquare />
+      <p className="flex items-center gap-2">
+        <FaRegCheckSquare className="min-w-3 min-h-3" />
         Постачання технічного обладнання (запасні частини та підмінний фонд).
       </p>
       <Devider text="Монтаж" />
-      <p className="mb-2">
+      <p>
         Наші висококваліфіковані фахівці перед проведенням монтажу виїжджають на
         об&apos;єкт, обговорюють місце розташування обладнання з урахуванням
         ваших вимог та відповідно до чинного законодавства. Вони встановлюють,
@@ -48,7 +49,7 @@ const Service = () => {
         завдання.
       </p>
       <Devider text="Обслуговування" />
-      <p className="mb-2">
+      <p>
         Для ефективної роботи та тривалого терміну служби обладнання необхідно
         регулярне технічне обслуговування. Оскільки таке обладнання часто працює
         в екстремальних умовах, температура та вологість робочого середовища
@@ -57,22 +58,28 @@ const Service = () => {
         позапланове (у разі потреби) обслуговування допоможе уникнути
         непотрібних простоїв, ремонтів та замін обладнання.
       </p>
-      <p className="mb-2">
+      <p>
         Наша компанія має сервісний центр, який надає гарантійне та
         післягарантійне обслуговування техніки з виїздом фахівців безпосередньо
         на об&apos;єкт.
       </p>
       <Devider text="Гарантія" />
-      <p className="mb-2">
+      <p>
         Ми надаємо офіційну гарантію на придбане обладнання протягом 12-18
         місяців.
       </p>
-      <p className="mb-2">
+      <p>
         Післягарантійне обслуговування та ремонт придбаного в нас обладнання
         здійснюються зі знижкою.
       </p>
-      <p className="my-10">
-        Контактний телефон сервісного центру: +38 (067) 123 45 67.
+      <p className="my-5 flex items-center gap-2">
+        Контактний телефон сервісного центру:
+        <Link
+          href="tel:+380631068008"
+          className="font-semibold transition hover:text-rose-700"
+        >
+          +38 063 106 80 08
+        </Link>
       </p>
     </section>
   );
--- components/Devider.tsx
+++ components/Devider.tsx
@@ -4,7 +4,7 @@ interface DeviderProps {
 
 export const Devider = ({ text }: DeviderProps) => {
   return (
-    <div className="relative flex justify-center my-4">
+    <div className="relative flex justify-center my-2">
       <p className="absolute inset-x-0 top-1/2 h-px -translate-y-1/2 bg-transparent bg-gradient-to-r from-transparent via-gray-500 to-transparent opacity-75"></p>
 
       <h2 className="relative z-10 bg-white font-semibold dark:bg-zinc-800 px-6">
