80799af39e19d638ce2d8648edf180f0cdb375cf
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2024-05-07 16:47:34 +0300

    Updated Contact-section

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -11,7 +11,7 @@ export const Contact = () => {
         alt="Світлина чоловіка перед ноутбуком з телефоном"
         className="rounded-lg w-full md:h-auto"
       />
-      <div className="flex flex-col gap-2 lg:gap-5 xl:gap-10 mx-auto max-w-screen-xl px-1 py-2 md:py-2 md:px-4 lg:p-5 xl:p-10 w-full">
+      <div className="flex flex-col gap-2 lg:gap-5 mx-auto max-w-screen-xl px-1 py-2 md:py-2 md:px-4 lg:p-5 xl:p-10 w-full">
         <h2 className="font-bold text-center lg:text-2xl">Є питання?</h2>
         <p className="text-gray-500">
           Залиште заявку, будь ласка. Наш менеджер зв&apos;яжеться з Вами
