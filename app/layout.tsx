078d8637a4e5f19d01075b29399dee243037e69b
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2024-05-18 18:20:35 +0300

    Added Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
@@ -42,6 +43,7 @@ export default function RootLayout({
             <div className="min-h-screen flex flex-col w-full max-w-[320px] md:max-w-[768px] lg:max-w-[1024px] xl:max-w-[1280px] mx-auto px-1">
               <Header />
               {children}
+              <Analytics />
               <Footer />
               <Toaster />
             </div>
